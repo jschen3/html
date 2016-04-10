82561392fad9048f372985b802378358346cd068
Jimmy Chen <chen.s.jimmy@gmail.com>
2016-04-09 22:38:30 -0600

    commiting a draft of the projects section

--- js/constants/constants.js
+++ js/constants/constants.js
@@ -1,4 +1,5 @@
 angular.module('Constants',[])
 	.constant('SLIDE_URL', 'http://jimmy-chen.net:8080/Website/slides')
 	.constant('ARTICLE_URL', 'http://jimmy-chen.net:8080/Website/articles')
-	.constant('IMAGE_URL', 'http://jimmy-chen.net:8080/Website/images');
+	.constant('IMAGE_URL', 'http://jimmy-chen.net:8080/Website/images')
+	.constant('PROJECT_URL', 'http://localhost:8080/Website/projects');
