8f18e84a3d36174d38cc22106d473e10ba2dfebe
Jimmy Chen <chen.s.jimmy@gmail.com>
2016-04-30 18:06:11 -0600

    made coding teasers sort by most recent day

--- js/controllers/teaserListApp.js
+++ js/controllers/teaserListApp.js
@@ -3,5 +3,8 @@ app.controller("teaserListCtrl",['$scope','teaserListFactory',
 	function($scope, teaserListFactory){
 	teaserListFactory.getTeasers().then(function(response){
 		$scope.teasers=response;
+		$scope.teasers.sort(function(teaser1,teaser2){
+			return teaser2.dateDay-teaser1.dateDay;
+		})
 	});
 }]);
